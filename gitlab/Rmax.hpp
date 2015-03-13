42a1fc8caef76a931485a7c2c74c013894affb50
Sveta <svetaarray@mail.ru>
2015-03-13 15:51:37 +0300

    +commentN

--- gitlab/Rmax.hpp
+++ gitlab/Rmax.hpp
@@ -2,19 +2,19 @@
 #define RMAX_HPP
 class Rmax
 {
-	public:
+	public://comment1
 	Rmax(); 
 	Rmax(double value); 
-	Rmax(Rmax& other); 
+	Rmax(Rmax& other); //comment2
 
 	Rmax operator+(Rmax other);
-	Rmax operator*(Rmax other);
+	Rmax operator*(Rmax other);//comment3
 	Rmax& operator=(Rmax other);
-	bool operator==(Rmax& other);
+	bool operator==(Rmax& other);//comment4
 
 	std::string toString();
 
-	double value;
+	double value;//comment5
 	static Rmax zero; 
 	static Rmax unit;
 };
